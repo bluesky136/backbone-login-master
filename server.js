9270a3eabd1b24083c47c53bfe7ccbb738353a79
John <convongz@gmail.com>
2015-09-20 01:46:04 +1000

    Update server.js

--- server.js
+++ server.js
@@ -13,12 +13,13 @@ connect().use(serveStatic(__dirname)).listen(8080);
 //
 var express = require('express'),
     http = require('http'),
-    bcrypt = require('bcrypt');
+	config = require("./config");
+ var  bcrypt = require('bcrypt');
 	bcrypt.genSalt(10, function(err, salt) {
     	bcrypt.hash('B4c0/\/', salt, function(err, hash) {
         // Store hash in your password DB.
     });	
-    config = require("./config"),
+    
  
 });
  
