cbc787aee4f9214f57aa7b86b6d446729c097d14
John <convongz@gmail.com>
2015-09-20 15:48:41 +1000

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ var express = require('express'),
 	bcrypt.genSalt(10, function(err, salt) {
     	bcrypt.hash('B4c0/\/', salt, function(err, hash) {
         // Store hash in your password DB.
+        */
     });	
- */   
+   
  
 });
  
